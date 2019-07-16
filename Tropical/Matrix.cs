34f1f0a457dd288b1726c5de3423a7463baa9591
ShTair <git@vsh.jp>
2019-07-16 18:13:48 +0900

    足し算実装

--- Tropical/Matrix.cs
+++ Tropical/Matrix.cs
@@ -1,4 +1,6 @@
-﻿namespace Tropical
+﻿using System;
+
+namespace Tropical
 {
     class Matrix
     {
@@ -20,6 +22,24 @@
             Height = h;
 
             _data = new double[w * h];
+        }
+
+        public static Matrix StandardAddition(Matrix a, Matrix b)
+        {
+            if (a.Width != b.Width || a.Height != b.Height) throw new Exception();
+
+            var result = new Matrix(a.Width, a.Height);
+            for (int i = 0; i < result._data.Length; i++)
+            {
+                result._data[i] = a._data[i] + b._data[i];
+            }
+
+            return result;
+        }
+
+        public static Matrix StandardMultiplication(Matrix a, Matrix b)
+        {
+
         }
     }
 }
