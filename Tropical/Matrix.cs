2281c6512e1d0e1382e0fed7c41b4d2743d71d80
ShTair <git@vsh.jp>
2019-07-16 17:17:22 +0900

    プロジェクト ファイルを追加します。

---
+++ Tropical/Matrix.cs
@@ -0,0 +1,25 @@
+﻿namespace Tropical
+{
+    class Matrix
+    {
+        private double[] _data;
+
+        public int Width { get; private set; }
+
+        public int Height { get; private set; }
+
+        public double this[int x, int y]
+        {
+            get { return _data[y * Width + x]; }
+            set { _data[y * Width + x] = value; }
+        }
+
+        public Matrix(int w, int h)
+        {
+            Width = w;
+            Height = h;
+
+            _data = new double[w * h];
+        }
+    }
+}
